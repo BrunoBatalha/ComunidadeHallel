291f83a9b335e10c004fc63fefc45c03a051c819
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2019-05-20 14:39:16 -0400

    yo

--- codigo/js/index.js
+++ codigo/js/index.js
@@ -1,4 +1,4 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() > 85) {
         document.getElementById('navbar').className = 'barraNav navbar-expand-md fixed-top';
         console.log($(this).scrollTop())
@@ -7,3 +7,41 @@ $(window).scroll(function() {
         console.log("saiu")
     }
 });
+
+
+$(window).scroll(function () {
+
+    if ($(this).scrollTop() > 85) {
+        document.getElementById('navbar').className = 'barraNav navbar-expand-md fixed-top';
+        console.log($(this).scrollTop())
+    } else {
+        document.getElementById('navbar').className = 'barraNav navbar-expand-md';
+        console.log("saiu")
+    }
+});
+
+function aumentaEsquerda(e) {
+    document.getElementById('marcadorEsquerdo').className = 'marcadorMidiaAbriu mt-5';
+}
+function diminuiEsquerda(e) {
+    document.getElementById('marcadorEsquerdo').className = 'marcadorMidia mt-5';
+}
+
+var elemento = document.getElementById("radio");
+elemento.addEventListener('mouseover', aumentaEsquerda);
+
+var elemento = document.getElementById("radio");
+elemento.addEventListener('mouseout', diminuiEsquerda);
+
+function aumentaDireita(e) {
+    document.getElementById('marcadorDireito').className = 'marcadorMidiaAbriu mb-5';
+}
+function diminuiDireita(e) {
+    document.getElementById('marcadorDireito').className = 'marcadorMidia mb-5';
+}
+
+var elemento = document.getElementById("tv");
+elemento.addEventListener('mouseover', aumentaDireita);
+
+var elemento = document.getElementById("tv");
+elemento.addEventListener('mouseout', diminuiDireita);

6630ac87169f7be37ca997afc4cf79872020d4fa
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2019-06-17 16:29:11 -0400

    curazon

--- codigo/js/pagamento.js
+++ codigo/js/pagamento.js
@@ -101,3 +101,16 @@ $("#cores-personalizadas").stepProgressBar(4);
 
 let temp = $("#cores-temperatura").stepProgressBar(3);
 $("#controle-temperatura").controlProgressBar(temp);
+
+function enviar_imagem(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#fotoPerfil').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#arquivo").change(function () {
+    enviar_imagem(this);
+});

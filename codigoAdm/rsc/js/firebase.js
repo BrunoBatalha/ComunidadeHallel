21cdf5eb0d387d9781cf4a4b879b18060e59d72d
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2019-09-17 15:15:03 -0400

    Create firebase.js

---
+++ codigoAdm/rsc/js/firebase.js
@@ -0,0 +1,13 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyCu0SEzl7pXz7eVwS5JD4qP5JIXIPcuc00",
+    authDomain: "comunidadehallel-2f0a6.firebaseapp.com",
+    databaseURL: "https://comunidadehallel-2f0a6.firebaseio.com",
+    projectId: "comunidadehallel-2f0a6",
+    storageBucket: "",
+    messagingSenderId: "1044647773691",
+    appId: "1:1044647773691:web:c60d66608737032b72b75e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+const rootRef = firebase.database().ref();
